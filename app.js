5d1128fff7a864f3f644e3d507cad640ee693b47
BumbleGoose <i.r.r.laurie@gmail.com>
2022-08-01 11:29:46 +0100

    add requiring of first controller

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 
+const { getAllCategories } = require(`${__dirname}/controllers/controllers.js`);
+
 app.use(express.json());
 
 app.get("/api/categories", getAllCategories);
