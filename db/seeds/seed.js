e3bf54694d98baff29afe5613876b7cfe1d0ecbd
BumbleGoose <i.r.r.laurie@gmail.com>
2022-08-01 11:53:27 +0100

    add modified pathway in seed.js to point to index file

--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -1,34 +1,34 @@
-const db = require("../connection");
+const db = require("../.");
 const format = require("pg-format");
 const {
-	convertTimestampToDate,
-	createRef,
-	formatComments,
+  convertTimestampToDate,
+  createRef,
+  formatComments,
 } = require("./utils");
 
 const seed = async (data) => {
-	const { categoryData, commentData, reviewData, userData } = data;
-	await db.query(`DROP TABLE IF EXISTS comments;`);
-	await db.query(`DROP TABLE IF EXISTS reviews;`);
-	await db.query(`DROP TABLE IF EXISTS users;`);
-	await db.query(`DROP TABLE IF EXISTS categories;`);
+  const { categoryData, commentData, reviewData, userData } = data;
+  await db.query(`DROP TABLE IF EXISTS comments;`);
+  await db.query(`DROP TABLE IF EXISTS reviews;`);
+  await db.query(`DROP TABLE IF EXISTS users;`);
+  await db.query(`DROP TABLE IF EXISTS categories;`);
 
-	const topicsTablePromise = db.query(`
+  const topicsTablePromise = db.query(`
   CREATE TABLE categories (
     slug VARCHAR PRIMARY KEY,
     description VARCHAR
   );`);
 
-	const usersTablePromise = db.query(`
+  const usersTablePromise = db.query(`
   CREATE TABLE users (
     username VARCHAR PRIMARY KEY,
     name VARCHAR NOT NULL,
     avatar_url VARCHAR
   );`);
 
-	await Promise.all([topicsTablePromise, usersTablePromise]);
+  await Promise.all([topicsTablePromise, usersTablePromise]);
 
-	await db.query(`
+  await db.query(`
   CREATE TABLE reviews (
     review_id SERIAL PRIMARY KEY,
     title VARCHAR NOT NULL,
@@ -41,7 +41,7 @@ const seed = async (data) => {
     votes INT DEFAULT 0 NOT NULL
   );`);
 
-	await db.query(`
+  await db.query(`
   CREATE TABLE comments (
     comment_id SERIAL PRIMARY KEY,
     body VARCHAR NOT NULL,
@@ -51,79 +51,79 @@ const seed = async (data) => {
     created_at TIMESTAMP DEFAULT NOW()
   );`);
 
-	const insertCategoriesQueryStr = format(
-		"INSERT INTO categories (slug, description) VALUES %L RETURNING *;",
-		categoryData.map(({ slug, description }) => [slug, description])
-	);
+  const insertCategoriesQueryStr = format(
+    "INSERT INTO categories (slug, description) VALUES %L RETURNING *;",
+    categoryData.map(({ slug, description }) => [slug, description])
+  );
 
-	const categoriesPromise = db
-		.query(insertCategoriesQueryStr)
-		.then((result) => result.rows);
+  const categoriesPromise = db
+    .query(insertCategoriesQueryStr)
+    .then((result) => result.rows);
 
-	const insertUsersQueryStr = format(
-		"INSERT INTO users ( username, name, avatar_url) VALUES %L RETURNING *;",
-		userData.map(({ username, name, avatar_url }) => [
-			username,
-			name,
-			avatar_url,
-		])
-	);
-	const usersPromise = db
-		.query(insertUsersQueryStr)
-		.then((result) => result.rows);
+  const insertUsersQueryStr = format(
+    "INSERT INTO users ( username, name, avatar_url) VALUES %L RETURNING *;",
+    userData.map(({ username, name, avatar_url }) => [
+      username,
+      name,
+      avatar_url,
+    ])
+  );
+  const usersPromise = db
+    .query(insertUsersQueryStr)
+    .then((result) => result.rows);
 
-	await Promise.all([categoriesPromise, usersPromise]);
+  await Promise.all([categoriesPromise, usersPromise]);
 
-	const formattedReviewData = reviewData.map(convertTimestampToDate);
+  const formattedReviewData = reviewData.map(convertTimestampToDate);
 
-	const insertReviewsQueryStr = format(
-		"INSERT INTO reviews (title, category, designer, owner, review_body, review_img_url, created_at, votes) VALUES %L RETURNING *;",
-		formattedReviewData.map(
-			({
-				title,
-				category,
-				designer,
-				owner,
-				review_body,
-				review_img_url,
-				created_at,
-				votes,
-			}) => [
-				title,
-				category,
-				designer,
-				owner,
-				review_body,
-				review_img_url,
-				created_at,
-				votes,
-			]
-		)
-	);
+  const insertReviewsQueryStr = format(
+    "INSERT INTO reviews (title, category, designer, owner, review_body, review_img_url, created_at, votes) VALUES %L RETURNING *;",
+    formattedReviewData.map(
+      ({
+        title,
+        category,
+        designer,
+        owner,
+        review_body,
+        review_img_url,
+        created_at,
+        votes,
+      }) => [
+        title,
+        category,
+        designer,
+        owner,
+        review_body,
+        review_img_url,
+        created_at,
+        votes,
+      ]
+    )
+  );
 
-	const reviewRows = await db
-		.query(insertReviewsQueryStr)
-		.then((result) => result.rows);
+  const reviewRows = await db
+    .query(insertReviewsQueryStr)
+    .then((result) => result.rows);
 
-	const reviewIdLookup = createRef(reviewRows, "title", "review_id");
-	const formattedCommentData = formatComments(commentData, reviewIdLookup);
+  const reviewIdLookup = createRef(reviewRows, "title", "review_id");
+  const formattedCommentData = formatComments(commentData, reviewIdLookup);
 
-	const insertCommentsQueryStr = format(
-		"INSERT INTO comments (body, author, review_id, votes, created_at) VALUES %L RETURNING *;",
-		formattedCommentData.map(
-			({ body, author, review_id, votes = 0, created_at }) => [
-				body,
-				author,
-				review_id,
-				votes,
-				created_at,
-			]
-		)
-	);
+  const insertCommentsQueryStr = format(
+    "INSERT INTO comments (body, author, review_id, votes, created_at) VALUES %L RETURNING *;",
+    formattedCommentData.map(
+      ({ body, author, review_id, votes = 0, created_at }) => [
+        body,
+        author,
+        review_id,
+        votes,
+        created_at,
+      ]
+    )
+  );
 
-	return db.query(insertCommentsQueryStr).then((result) => {
-		return result.rows;
-	});
+  return db.query(insertCommentsQueryStr).then((result) => {
+    return result.rows;
+  });
 };
 
 module.exports = seed;
